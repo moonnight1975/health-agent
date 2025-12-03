72f35b4315b0c944c393ef76004942c64e58c46a
Moonnight1975 <sibijacob69@gmail.com>
2025-12-03 22:11:19 +0530

    updated commit

--- app/components/MoodLottie.tsx
+++ app/components/MoodLottie.tsx
@@ -1,6 +1,10 @@
 'use client'
 
-import { Player } from '@lottiefiles/react-lottie-player'
+import dynamic from 'next/dynamic'
+
+const Player = dynamic(() => import('@lottiefiles/react-lottie-player').then(mod => mod.Player), {
+    ssr: false
+})
 
 export default function MoodLottie() {
     return (
